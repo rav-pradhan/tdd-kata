bb8b170d92536ae10d6b356416154170c648c12b
Ravi <ravipradhan@protonmail.com>
2020-11-13 23:36:09 +0000

    mumbling refactor

--- mumbling/js/mumble.js
+++ mumbling/js/mumble.js
@@ -2,20 +2,10 @@ export default function mumbleLetters(string = "") {
   return buildMumbledLettersArray(string).join('-')
 }
 
-const buildMumbledLettersArray = (string = "") => {
+const buildMumbledLettersArray = (string) => {
   return string.split("").map((character, index) => {
-    let mumbledEntry = [];
-    mumbledEntry.push(repeatLetter(character, index))
+    let mumbledEntry = []
+    mumbledEntry.push(`${character.toUpperCase()}${character.toLowerCase().repeat(index)}`)
     return mumbledEntry
   })
-}
-
-const repeatLetter = (letter = '', numberOfTimes = 0) => {
-  let repeated = [];
-  
-  for (let i = 0; i < numberOfTimes + 1; i++) {
-    i === 0 ? repeated.push(letter.toUpperCase()) : repeated.push(letter.toLowerCase());
-  }
-
-  return repeated.join('');
 }
